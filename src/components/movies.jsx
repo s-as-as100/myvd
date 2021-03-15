0f51d2f4fc86c495dc6c5ba6822bfe0b68cb77c7
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-15 12:46:54 +0530

    extracting MoviesTable

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Like from './common/like';
+import MoviesTable from './moviesTable';
  import { getMovies} from './../services/fakeMovieService';
 import Pagination from './common/pagination';
 import {paginate} from './utils/paginate';
 import { getGenres } from '../services/fakeGenreService';
 import ListGroup from './common/listGroup';
+import MOviesTable from './moviesTable';
 
  class Movies extends Component {
      state = { 
@@ -60,31 +61,8 @@ import ListGroup from './common/listGroup';
                   </div>
                   <div className="col">
                   <p>Showing {filtered.length} movies in the database</p>
-             <table className="table">
-                 <thead>
-                     <tr>
-                         <th>Title</th>
-                         <th>Genre</th>
-                         <th>Stock</th>
-                         <th>Rate</th>
-                         <th>Like</th>
-                         <th></th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     {movies.map(movie => (
-                         <tr key={movie._id}>
-                         <td>{movie.title}</td>
-                         <td>{movie.genre.name}</td>
-                         <td>{movie.numberInStock}</td>
-                         <td>{movie.dailyRentalRate}</td>
-                         <td><Like  liked ={movie.liked} onClick = { () => this.handleLike(movie)} /></td>
-                         <td><button onClick ={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                     </tr>
-                     ))}
-                     
-                 </tbody>
-             </table>
+                  <MOviesTable movies = {movies} onLike={this.handleLike} onDelete={this.handleDelete}  />
+           
              <Pagination 
              itemsCount =  {filtered.length} 
              pageSize = {pageSize} 
